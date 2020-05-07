029c0b3f6c81815b7f65132c2ae00d26bfa9b9a1
Akash Sharma <aakashsharmajc100@gmail.com>
2020-05-07 19:21:42 +0530

    Debug turned on

--- docs/settings.py
+++ docs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ktud))ux7imlkr$d(frj$9xf)#3izr68-+$%al2y$uk)aokn9e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
